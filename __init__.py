4518502a1c1895c43208c010142f8f70283cb520
j-raghavan <mr.jayasimha@gmail.com>
2024-02-19 22:42:59 -0800

    Adding initial commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# Author: J Raghavan 
+# Created on: 02.19.2024
+# Last modified on: 02.19.2024
+
+# This package implements a simple signal/slot mechanism for communication between objects.
+# Signals and slots is a language construct introduced in Qt[1] for communication between 
+# objects which makes it easy to implement the observer pattern while avoiding boilerplate 
+# code. A python object can send signals containing event information which can be received by other objects 
+# using special member functions known as slots.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
